3aa9d37e2513c55e68a42b02d85bbbd6e04c9042
ivanrmn <ivanrmnh@gmail.com>
2019-01-09 17:20:56 +0100

    wip

---
+++ Meeseeks.js
@@ -0,0 +1,33 @@
+class Meeseeks {
+    constructor(mensajeInicial, mensajeAcaba, mensajeExplota) {
+      this.mensajeInicial = mensajeInicial;
+      this.mensajeAcaba = mensajeAcaba;
+      this.mensajeExplota = mensajeExplota;
+    }
+  }
+
+function meeseeks(){
+  var meeseeksOriginales = [];
+  var meeseeksNuevos = [];
+
+  for(
+}
+
+// Acción y objeto sobre el que la realizan
+  function makeRequest(accion, objetoUsado){
+    return mensajeAcaba;
+  }
+
+  function singleBox(){
+
+  }
+
+// node.js modules
+exports.singletonBox = function() {
+	// esta es la funcion a exportar
+    return singleBox();
+};
+// -------------------------
+// Asi se usa:
+var importaBox = require('./box');
+var factoriaBox = importaBox.singletonBox();
